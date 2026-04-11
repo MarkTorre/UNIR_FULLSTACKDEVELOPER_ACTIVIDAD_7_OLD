c6a2da90fc4ad896f627a9e1e02dc5a66aa4cfe1
MarkTorre <marc.torrecilla.mate@gmail.com>
2026-04-11 19:03:29 +0200

    feat(sql_script): finished sql script

--- Apartado1/SQL_Init_DB_Script.sql
+++ Apartado1/SQL_Init_DB_Script.sql
@@ -1,11 +1,22 @@
 #INSERTAR USUARIO
-INSERT INTO usuarios(nombre, email, contraseña) VALUES ("Andrea","andrea@gmail.com","A4N5D6R7");
+INSERT INTO usuarios(id, nombre, email, contraseña) VALUES (0,"Andrea","andrea@gmail.com","A4N5D6R7");
+
 #INSERTAR ARTISTA
-INSERT INTO artistas(nombre, foto) VALUES ("Brian Eno", "/public/artistas/brian-eno/brian_eno_profile.jpg");
+INSERT INTO artistas(id, nombre, foto) VALUES (0,"Brian Eno", "/public/artistas/brian-eno/brian_eno_profile.jpg");
+
 #INSERTAR ALBUM
-INSERT INTO albumes(titulo, discografica, año) VALUES ("Secret Life","Text Records",2026);
+INSERT INTO albumes(id, titulo, discografica, año) VALUES (0,"Secret Life","Text Records",2026);
+
 #INSERTAR TRES CANCIONES
-INSERT INTO canciones(titulo, genero, año) VALUES ("","Ambient",2026);
+INSERT INTO canciones(id, titulo, genero, año) VALUES (0,"I Saw You","Ambient",2023);
+INSERT INTO canciones(id, titulo, genero, año) VALUES (1,"Sunset","Classic",2016);
+INSERT INTO canciones(id, titulo, genero, año) VALUES (2,"Empire Systems","Black knoll",2015);
+
 #CREAR LISTA CON TRES CANCIONES QUE ACABAS DE DAR DE ALTA
-INSERT INTO listas(nombre, fd_usuario)
+INSERT INTO listas(id, nombre, fk_usuario) VALUES (0, "MiLista1", 0);
+INSERT INTO tbi_listas_canciones (id, fk_listas, fk_canciones) VALUES (0, 0, 0);
+INSERT INTO tbi_listas_canciones (id, fk_listas, fk_canciones) VALUES (1, 0, 1);
+INSERT INTO tbi_listas_canciones (id, fk_listas, fk_canciones) VALUES (2, 0, 2);
+
 #REGISTRAR QUE EL USUARIO HA ESCUCHADO UNA DE LAS TRES CANCIONES
+INSERT INTO tbi_usuarios_canciones (id, fk_usuarios, fk_canciones) VALUES (0, 0, 0)
